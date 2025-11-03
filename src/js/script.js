17c4e2c90cae20e9286e6ef86c4e11794029c861
Natalja Ray <natalja_fb@mail.ru>
2025-11-03 20:58:08 +0300

    develop: upd 03-11

--- src/js/script.js
+++ src/js/script.js
@@ -28,8 +28,8 @@ $('#art-carousel').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                // slidesToScroll: 3,
                 infinite: true,
                 // dots: false
             }
@@ -37,24 +37,105 @@ $('#art-carousel').slick({
         {
             breakpoint: 767,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                // slidesToScroll: 2
             }
         },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+        // {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //     }
+        // }
     ]
 });
 
+// guides-carousel
+$('#guides-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1386,
+            settings: {
+                slidesToShow: 3,
+                // slidesToScroll: 3,
+                infinite: true,
+                // dots: false
+            }
+        },
+        {
+            breakpoint: 1060,
+            settings: {
+                slidesToShow: 2,
+                // slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                // slidesToScroll: 1
+            }
+        }
+    ]
+});
 
+// book-carousel
+$('#book-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+// volunteers-carousel
+$('#volunteers-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1386,
+            settings: {
+                slidesToShow: 3,
+                // slidesToScroll: 3,
+                infinite: true,
+                // dots: false
+            }
+        },
+        {
+            breakpoint: 1060,
+            settings: {
+                slidesToShow: 2,
+                // slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                // slidesToScroll: 1
+            }
+        }
+    ]
+});
 
 // /**
 //  * platform
