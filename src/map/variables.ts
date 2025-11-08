f9a9c7290d1b5e58a1ca65bcb17476f0fca0eaf1
NataljaRay <ray92v@gmail.com>
2025-11-08 17:54:54 +0300

    yandex-map: done 1

--- src/map/variables.ts
+++ src/map/variables.ts
@@ -2,22 +2,15 @@ import type {LngLat, YMapLocationRequest} from '@yandex/ymaps3-types';
 import type {YMapDefaultMarkerProps} from '@yandex/ymaps3-default-ui-theme';
 
 export const LOCATION: YMapLocationRequest = {
-    // center: [39.7112, 43.5797], // starting position [lng, lat]
-    // zoom: 14.5 // starting zoom
-
-    // center: [49.130081, 55.819951],
     center: [49.145081, 55.806951],
-    // zoom: 13
-    zoom: 12
+    zoom: 13
 };
 
 export const FIRST_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; coordinates: LngLat} = {
     // iconName: 'star',
-    iconName: 'none',
     coordinates: [49.120607, 55.822028]
 };
 export const SECOND_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; coordinates: LngLat} = {
-    // iconName: 'waterpark',
-    iconName: 'star',
+    // iconName: 'star',
     coordinates: [49.173725, 55.797217]
 };
