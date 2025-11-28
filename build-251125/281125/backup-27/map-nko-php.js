4c3be179c4dce9c8911a7c00eef9b84b35eff7ca
NataljaRay <ray92v@gmail.com>
2025-11-28 10:04:01 +0300

    bitrix-map-final-3.0: save 28.11

---
+++ build-251125/281125/backup-27/map-nko-php.js
@@ -0,0 +1,195 @@
+// import { FIRST_MARKER_PROPS, SECOND_MARKER_PROPS, LOCATION } from 'map/variables';
+import { InfoMessage } from 'map/common';
+
+window.map = null;
+
+main();
+
+async function main() {
+    await ymaps3.ready;
+
+    const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapControls, YMapMarker } = ymaps3;
+
+    // ------------------------
+    // 1. Загружаем данные из JSON
+    // ------------------------
+
+    // const MARKERS = await fetch('map/events-nko.json').then(r => r.json());
+    const MARKERS = window.MARKERS_FROM_BITRIX;
+    // console.log('MARKERS', MARKERS)
+
+    // ------------------------
+    // 2. Создаём карту
+    // ------------------------
+
+    const isMobile = window.matchMedia('(max-width: 480px)').matches;
+    const isIpad = window.matchMedia('(max-width: 1023px)').matches;
+
+    // const mapCenter = isMobile ? [49.175081, 55.796951] : [49.145081, 55.790951];
+    // const mapZoom   = isMobile ? 11 : 12;
+    // const mapCenter = isMobile ? [49.075081, 55.796951] : (isIpad ? [49.045081, 55.796951] : [49.005081, 55.799951]);
+    // const mapZoom   = isMobile ? 9 : (isIpad ? 10 : 11);
+
+    const mapCenter = isMobile ? [50.575081, 55.796951] : (isIpad ? [50.445081, 55.796951] : [50.805081, 55.799951]);
+    const mapZoom   = isMobile ? 6 : (isIpad ? 7 : 8);
+
+
+    const map = new YMap(
+        document.getElementById('map'),
+        {
+            location: { center: mapCenter, zoom: mapZoom }
+        },
+        [
+            new YMapDefaultSchemeLayer(),
+            new YMapDefaultFeaturesLayer()
+        ]
+    );
+
+    // ------------------------
+    // 3. InfoMessage — контейнер для попапа
+    // ------------------------
+
+    const infoMessage = new InfoMessage({ text: '' });
+
+    map.addChild(
+        new YMapControls({ position: 'top right' }).addChild(infoMessage)
+    );
+
+    const mapContainer = document.getElementById('map');
+
+    // ------------------------
+    // 4. Создаём единственный попап
+    // ------------------------
+
+    const popupElement = document.createElement('div');
+    popupElement.classList.add('map-popup', 'map-popup--hidden');
+
+    popupElement.innerHTML = `
+        <div class="map-popup__content">
+            <div class="map-popup__list"></div>
+            <button class="map-popup__close">Закрыть</button>
+        </div>
+    `;
+
+    infoMessage.updateContent(popupElement);
+
+    const popupContent = popupElement.querySelector('.map-popup__content');
+    const popupList = popupElement.querySelector('.map-popup__list');
+    const closeButton = popupElement.querySelector('.map-popup__close');
+
+    // ------------------------
+    // 5. Рендер мероприятий в попап
+    // ------------------------
+
+    function updatePopup(eventsArray) {
+        const popupHtmlBase = eventsArray.map(event => `
+            <div class="map-popup__event" id="event-${event.eventId}">
+                <div class="map-popup__title">${event.title}</div>
+
+                <div class="map-popup__info">
+                    <div class="map-popup__info-img-wrap">
+                        <img class="map-popup__info-img"
+                             src="${event.image}"
+                             alt="${event.imageAlt}">
+                    </div>
+                    <div class="map-popup__info-text">
+                      <div id="when"></div>
+                      <div id="time"></div>
+                      <div id="address"></div>
+                    </div>
+                </div>
+
+                <div class="map-popup__text">
+                    <div class="map-popup__text-content">${event.text}</div>
+                </div>
+            </div>
+        `).join("");
+
+        popupList.innerHTML = popupHtmlBase;
+
+        const popupInfoWhen = document.querySelector('#when');
+        const popupInfoTime = document.querySelector('#time');
+        const popupInfoAddress = document.querySelector('#address');
+
+        if(eventsArray[0].when && (eventsArray[0].when != '')){
+            popupInfoWhen.innerHTML = 'Когда: <span>' + eventsArray[0].when + '</span>';
+        }
+
+        if(eventsArray[0].time && (eventsArray[0].time != '')){
+            popupInfoTime.innerHTML = 'Во сколько: <span>' + eventsArray[0].time + '</span>';
+        }
+
+        if(eventsArray[0].address && (eventsArray[0].address != '')){
+            popupInfoAddress.innerHTML = 'Где: <span>' + eventsArray[0].address + '</span>';
+        }
+
+        popupElement.classList.remove('map-popup--hidden');
+
+        // сброс скролла наверх
+        popupContent.scrollTop = 0;
+    }
+
+    function closeInfo() {
+        popupElement.classList.add('map-popup--hidden');
+        changePointColor();
+    }
+
+    // ------------------------
+    // 6. Обработчики закрытия попапа
+    // ------------------------
+
+    closeButton.addEventListener('click', (e) => {
+        e.stopPropagation();
+        closeInfo();
+    });
+
+    popupElement.addEventListener('click', e => e.stopPropagation());
+    mapContainer.addEventListener('click', closeInfo);
+
+    // ------------------------
+    // 7. Создание маркера
+    // ------------------------
+
+    function createCustomMarker(markerData) {
+
+        const markerEl = document.createElement('div');
+        markerEl.className = 'map-point';
+        markerEl.dataset.markerId = markerData.id;
+
+        markerEl.innerHTML = `
+        <div class="map-point__inner">
+            <span class="map-point__counter">${markerData.events.length}</span>
+        </div>
+    `;
+
+        const marker = new YMapMarker(
+            { coordinates: markerData.coords },
+            markerEl
+        );
+
+        markerEl.addEventListener('click', (e) => {
+            e.stopPropagation();
+
+            changePointColor();
+            markerEl.classList.add('map-point--current');
+
+            updatePopup(markerData.events);
+        });
+
+        return marker;
+    }
+
+    function changePointColor() {
+        document
+            .querySelectorAll('.map-point')
+            .forEach(p => p.classList.remove('map-point--current'));
+    }
+
+    // ------------------------
+    // 8. Добавляем маркеры
+    // ------------------------
+
+    MARKERS.forEach(markerData => {
+        map.addChild(createCustomMarker(markerData));
+    });
+}
---
+++ build-251125/281125/index.php
@@ -0,0 +1,303 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Щедрый Щак Щак");
+
+$APPLICATION->AddHeadString('<script crossorigin src="https://cdn.jsdelivr.net/npm/@babel/standalone@7/babel.min.js"></script>');
+$APPLICATION->AddHeadString('<script src="https://api-maps.yandex.ru/v3/?apikey=9396485d-1500-4279-8445-c5db0367ddf1&lang=ru_RU"></script>');
+$APPLICATION->AddHeadString('<script src="https://forms.yandex.ru/_static/embed.js"></script>');
+
+
+$APPLICATION->AddHeadString('<link rel="preload" href="fonts/NauryzRedKeds.woff2" as="font" type="font/woff2" crossorigin>');
+$APPLICATION->AddHeadString('<link rel="preload" href="img/pyramid.svg" as="image">');
+$APPLICATION->AddHeadString('<link rel="preload" href="img/tuesday.svg" as="image">');
+$APPLICATION->AddHeadString('<link rel="preload" href="img/bg-full/heart-white.svg" as="image">');
+$APPLICATION->AddHeadString('<style>#chak-chak {font-family: \'Inter\', sans-serif;font-weight: 400;}</style>');
+$APPLICATION->SetAdditionalCSS("/culture-of-charity/shchedryy-shchak-shchak/css/style.css");
+?>
+
+<div id="chak-chak">
+  <div id="participation" class="container">
+    <span id="participation-btn">Прими участие</span>
+    <div class="participation-popup-wrapper participation-popup-wrapper--hidden" id="participation-popup">
+      <div class="slide-popup participation-popup participation-popup--hidden">
+        <div class="map-popup__content">
+          <div class="map-popup__list">
+            <iframe src="https://forms.yandex.ru/u/6921bfebd046880445c04b79?iframe=1" frameborder="0" name="ya-form-6921bfebd046880445c04b79" width="100%" height="100%"></iframe>
+          </div>
+          <button class="map-popup__close">Закрыть</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="background-linear--1">
+    <section class="section--main container">
+      <div class="hero-3">
+        <div class="hero-3__intro">
+
+          <div class="hero-3__pyramid">
+            <img class="hero-3__pyramid-img" src="img/pyramid.svg" width="224" height="182" alt="Щак-щак">
+            <h1 class="hero-3__pyramid-txt">Щедрый <br><span>Щак-Щак</span></h1>
+          </div>
+
+          <div class="hero-3__info">
+            <div class="hero-3__dates">
+              1<sup>11</sup> — 31<sup>12</sup>
+            </div>
+            <p>Щедрый Щак-Щак — ежегодный праздник<br> благотворительности в Татарстане. В это время<br> миллионы людей
+              рассказывают, как и кому они помогают.<br> Корни Щедрого Щак-Щака — благотворительный день Щедрый<br> Вторник
+              – день, когда в России люди помогают тем, кому необходимо.</p>
+          </div>
+        </div>
+
+        <div class="hero-3__block">
+          <div class="hero-3__block-part-1">
+            <img class="hero-3__block-img" src="img/tuesday.svg" width="210" height="71" alt="Щедрый вторник">
+            <div class="hero-3__char hero-3__char--plus">+</div>
+            <h2 class="hero-3__txt">Татарстан</h2>
+          </div>
+          <div class="hero-3__block-part-2">
+            <div class="hero-3__char hero-3__char--equals">=</div>
+            <h2 class="hero-3__pyramid-txt">Щедрый <br><span>Щак-Щак</span></h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section--map container">
+      <h3 class="section__title section__title--cold animate__animated">Карта мероприятий</h3>
+      <div class="map" aria-hidden="true">
+        <div class="map__inner" id="map"></div>
+      </div>
+    </section>
+
+    <div class="slide-popup-wrapper slide-popup-wrapper--hidden">
+      <div id="slide-popup" class="slide-popup map-popup map-popup--hidden">
+        <div class="map-popup__content">
+          <div class="map-popup__list"></div>
+          <button class="map-popup__close">Закрыть</button>
+        </div>
+      </div>
+    </div>
+
+    <?
+    /*
+        Щедрый Бизнес
+    */
+    $APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "shchedryy-biznes",
+        array(
+            "IBLOCK_ID" => "28",
+            "IBLOCK_TYPE" => "news", // укажите ваш тип инфоблока
+            "NEWS_COUNT" => "10000", // количество новостей
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_ORDER1" => "DESC",
+            "FIELD_CODE" => array("NAME", "PREVIEW_TEXT"),
+            "PROPERTY_CODE" => array(),
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "3600",
+            "SET_BROWSER_TITLE"=>"N",
+            "SET_TITLE"=>"N"
+        )
+    );?>
+
+    <?
+    /*
+        Щедрые НКО
+    */
+    $APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "nko",
+        array(
+            "IBLOCK_ID" => "25",
+            "IBLOCK_TYPE" => "news", // укажите ваш тип инфоблока
+            "NEWS_COUNT" => "10000", // количество новостей
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_ORDER1" => "DESC",
+            "FIELD_CODE" => array("NAME", "PREVIEW_TEXT"),
+            "PROPERTY_CODE" => array("DATE", "ADDRESS"),
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "3600",
+            "SET_BROWSER_TITLE"=>"N",
+            "SET_TITLE"=>"N"
+        )
+    );?>
+
+  </div>
+
+
+<?
+/*
+	Щедрый арт
+*/
+$APPLICATION->IncludeComponent(
+  "bitrix:news.list",
+  "art",
+  array(
+    "IBLOCK_ID" => "26",
+    "IBLOCK_TYPE" => "news", // укажите ваш тип инфоблока
+    "NEWS_COUNT" => "10000", // количество новостей
+    "SORT_BY1" => "ACTIVE_FROM",
+    "SORT_ORDER1" => "DESC",
+    "FIELD_CODE" => array("NAME", "PREVIEW_TEXT"),
+    "PROPERTY_CODE" => array(),
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "3600",
+	"SET_BROWSER_TITLE"=>"N",
+	"SET_TITLE"=>"N"
+  )
+);?>
+  <div class="background-linear--2">
+<?
+/*
+	Щедрые волонтеры
+*/
+$APPLICATION->IncludeComponent(
+  "bitrix:news.list",
+  "volunteers",
+  array(
+    "IBLOCK_ID" => "27",
+    "IBLOCK_TYPE" => "news", // укажите ваш тип инфоблока
+    "NEWS_COUNT" => "10", // количество новостей
+    "SORT_BY1" => "ACTIVE_FROM",
+    "SORT_ORDER1" => "DESC",
+    "FIELD_CODE" => array("NAME", "PREVIEW_TEXT"),
+    "PROPERTY_CODE" => array(),
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "3600",
+	"SET_BROWSER_TITLE"=>"N",
+	"SET_TITLE"=>"N"
+  )
+);?>
+  </div>
+
+  <div class="background-white">
+  
+<?php
+/*
+	Щедрые районы
+*/
+$APPLICATION->IncludeComponent(
+  "bitrix:news.list",
+  "street",
+  array(
+    "IBLOCK_ID" => "24",
+    "NEWS_COUNT" => "10",
+    "SORT_BY1" => "ACTIVE_FROM",
+    "SORT_ORDER1" => "DESC",
+    "FIELD_CODE" => array("NAME", "PREVIEW_TEXT"),
+    "PROPERTY_CODE" => array(),
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "36000000",
+	"SET_BROWSER_TITLE"=>"N",
+	"SET_TITLE"=>"N"
+  ),
+  false
+);
+?>
+
+    <section class="section--guides section--guides-copy container">
+      <div class="guides">
+        <h3 class="section__title animate__animated">Щедрые <br>экскурсоводы</h3>
+        <div class="guides__inner">
+          <div class="guides-carousel" id="guides-carousel">
+
+            <div class="guides-carousel__slide slide">
+              <div class="guides-carousel__slide-inner">
+                <div class="guides-carousel__slide-img-wrap slide-img-wrap">
+                  <img class="guides-carousel__slide-img slide-img" src="img/guides/g-1-Valeeva.jpeg" alt="Изображение">
+                </div>
+                <div class="guides-carousel__slide-info slide-info">
+                  <p class="slide-title">Эльвира Валеева</p>
+                  <p class="slide-txt">
+                    Благотворительная экскурсия "Казань культурная: от изобразительного до театрального искусства"
+                  </p>
+                  <p class="slide-txt">
+                    24 ноября 2025г, 12:00
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="guides-carousel__slide slide">
+              <div class="guides-carousel__slide-inner">
+                <div class="guides-carousel__slide-img-wrap slide-img-wrap">
+                  <img class="guides-carousel__slide-img slide-img" src="img/guides/g-2-Minnulina.jpeg" alt="Изображение">
+                </div>
+                <div class="guides-carousel__slide-info slide-info">
+                  <p class="slide-title">Раушания Миннулина </p>
+                  <p class="slide-txt">
+                    Благотворительная экскурсия по Музею-заповеднику «Казанский Кремль»
+                  </p>
+                  <p class="slide-txt">
+                    27 ноября 2025г, 11:00
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="guides-carousel__slide slide">
+              <div class="guides-carousel__slide-inner">
+                <div class="guides-carousel__slide-img-wrap slide-img-wrap">
+                  <img class="guides-carousel__slide-img slide-img" src="img/guides/g-3-Hmilova.jpeg" alt="Изображение">
+                </div>
+                <div class="guides-carousel__slide-info slide-info">
+                  <p class="slide-title">Наталья Хмылова</p>
+                  <p class="slide-txt">
+                    Благотворительная экскурсия в Раифском Богородицком монастыре
+                  </p>
+                  <p class="slide-txt">
+                    26 ноября 2025г, 10:00
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</div>
+<style>
+  body>.container {
+    max-width: 100%;
+  }
+
+  /*#panel + .container {position: fixed;top: 0;z-index: 99999;}*/
+  /*.header.fixed {width: 100%;position: relative;}*/
+  .header.fixed {
+    width: 100%;
+  }
+
+  .head_flex {
+    max-width: 1440px;
+  }
+
+  @media screen and (min-width: 1455px) {
+    .head_flex {
+      max-width: 1296px;
+    }
+  }
+
+  #chak-chak {
+    overflow-x: hidden;
+  }
+</style>
+
+<script src="/culture-of-charity/shchedryy-shchak-shchak/assets/slick/slick.min.js"></script>
+<script src="/culture-of-charity/shchedryy-shchak-shchak/bundle.js"></script>
+<script src="/culture-of-charity/shchedryy-shchak-shchak/custom.js"></script>
+
+
+<script
+  data-plugins="transform-modules-umd"
+  data-presets="react, typescript"
+  type="text/babel"
+  src="map/common.ts"></script>
+<script
+  data-plugins="transform-modules-umd"
+  data-presets="react, typescript"
+  type="text/babel"
+  src="map/map-nko.js"></script>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
